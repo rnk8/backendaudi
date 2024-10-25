1cbd762a7e507959e7b2d39201ebe1f5f32965ae
rnk8 <shidoriicefrog98@gmail.com>
2024-10-25 03:48:04 -0600

    Added bcrypt de21341231212312331212312341231233

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432, // Normalmente es 5432
-  connectionTimeoutMillis: 15000, // Ajusta el tiempo de espera si es necesario
+  user: process.env.DB_USER,          // 'ren'
+  host: process.env.DB_HOST,          // 'backendaudi.onrender.com'
+  database: process.env.DB_DATABASE,  // 'audi'
+  password: process.env.DB_PASSWORD,  // '0808'
+  port: process.env.DB_PORT,          // 5432
+  connectionTimeoutMillis: 15000,     // Ajusta el tiempo de espera si es necesario
 });
 
 pool.query('SELECT NOW()', (err, res) => {
