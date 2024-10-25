68d05def73c66ca101b6561f5b4901f8e794101d
rnk8 <shidoriicefrog98@gmail.com>
2024-10-25 03:33:27 -0600

    Added bcrypt de2134123123

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import authRoutes from './server/routes/authRoutes.js'; // Asegúrate de que esta ruta sea correcta
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000; // Usa el puerto de la variable de entorno o 4000 como predeterminado
 
 app.use(cors({
-  origin: 'https://auditoria-murex.vercel.app', // Asegúrate de especificar el origen correcto
-  credentials: true,
+  origin: 'https://auditoria-murex.vercel.app',
 }));
-app.use(bodyParser.json());
+app.use(express.json()); // Para parsear application/json
+app.use(express.urlencoded({ extended: true })); // Para parsear application/x-www-form-urlencoded
 
 // Configura las rutas de autenticación
 app.use('/api/auth', authRoutes);
+
+// Middleware de manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
